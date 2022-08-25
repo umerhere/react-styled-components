a39deb290f56f7f7084d150d36855d2cb6fb9508
Umar Aslam <umar.aslam@RT-LT-606.rolustech.net>
2022-08-25 12:33:10 +0500

    add global styles using styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components';
 import Header from './components/Header';
-import { Container } from './components/styles/Container.styled';
+import { Container } from './components/styles/Container.styled'; //used { } cz Continer was not exported as default
+import GlobalStyles from './components/styles/Global.styled'; //not used { } cz GlobalStyles was exported as default
 
 const theme = {
   colors: {
@@ -14,6 +15,7 @@ function App() {
   return (
     <ThemeProvider theme = {theme}>
       <>
+        <GlobalStyles />
         <Header />
         <Container>
           <h1>Hello world</h1>
